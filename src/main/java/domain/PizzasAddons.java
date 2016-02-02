b21deb9c331ade1f573e586c1673f77862c905da
PavlivAndriy <andriypavliv1@gmail.com>
2016-02-02 22:11:07 +0200

    Added enums, because there was some issue.

---
+++ src/main/java/domain/PizzasAddons.java
@@ -0,0 +1,8 @@
+package domain;
+
+/**
+ * Created by Andriy on 2/2/2016.
+ */
+public enum  PizzasAddons {
+    CHEESE, SAUSAGE, SPICE, FRUITS, TOMATO
+}
---
+++ src/main/java/domain/PizzasNames.java
@@ -0,0 +1,8 @@
+package domain;
+
+/**
+ * Created by Andriy on 2/2/2016.
+ */
+public enum PizzasNames {
+    CAPRICCIOSA, SALAMI, VEGETERIANA, MEXICANO, PAPPERONI
+}
